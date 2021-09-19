0b6db5011aade772ddd122005441c8e1f870541e
eyal <eyal5000192@gmail.com>
2021-09-19 20:54:23 +0300

    js

--- script/1.js
+++ script/1.js
@@ -1,40 +1,8 @@
- function init()
- {
-     
- }
-
- function f1()
- {
-    let result1=new Array();
-
-    result1[0]=document.getElementById("one").value;
-    result1[1]=document.getElementById("two").value;
-
-    if((result1[0]==7)&&(result1[1])==5)
-        alert("good");
-    else
-        alert("NO");
- }
-
- function detailHundred()
- {
-    document.getElementById('one1').src="../../images/ziro.png";
-    document.getElementById('ziro1').src="../../images/ten.png";
- }
-
- function detailTen()
- {
-      
-     
-      document.getElementById('ziro1').src="../../images/nine.png";
-      document.getElementById('ziro2').src="../../images/ten.png";
-    
- }
-
+ 
  function allowDrop(ev)
-  {
+ {
     ev.preventDefault();
-  }
+ }
   
   function drag(ev) 
   {
