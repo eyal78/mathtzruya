0d4227b07ba6919dc9e1245ff4e5a6c9e980405b
eyal <eyal5000192@gmail.com>
2022-06-27 10:54:38 +0300

    1

--- script/1.js
+++ script/1.js
@@ -1,9 +1,9 @@
  function init()
-  {
+ {
 
-  }
+ }
  function drop(ev) 
-  {
+ {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
@@ -14,19 +14,19 @@
      }
     else
       alert("hello!")
-  }
+ }
  function allowDrop(ev)
-  {
+ {
     ev.preventDefault();    
-  }
+ }
   
  function drag(ev) 
-  {
+ {
     ev.dataTransfer.setData("text", ev.target.id);   
-  }
+ }
   
-  function one()
-  {
+ function one()
+ {
     var x=document.getElementById("one");
     window.alert(x);
     document.getElementById("two").innerHTML="welcome";
@@ -34,4 +34,4 @@
       document.getElementById("re_one").src='../images/good.png';
     else
       document.getElementById("re_one").src='../images/unity.png';
-  }
+ }
